28139782deb51fd2c8b0478d4335c9c4aa6398b8
muratilgun <muratilgun34@gmail.com>
2022-05-14 19:07:10 +0300

    Grpc Configurations

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,12 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Discount.API.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Discount.API
 {
@@ -14,9 +8,9 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-           var host =  CreateHostBuilder(args).Build();
-           host.MigrateDatabase<Program>();
-           host.Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
