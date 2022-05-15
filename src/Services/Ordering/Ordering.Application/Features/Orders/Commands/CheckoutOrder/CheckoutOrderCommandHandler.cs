5ec789494072394614ec9328b1c7aea69364f5bd
muratilgun <muratilgun34@gmail.com>
2022-05-15 22:14:06 +0300

    Create CheckoutOrderCommandHandler.cs

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand,int>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
