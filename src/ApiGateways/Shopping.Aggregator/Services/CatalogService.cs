eb7a36da89aae232169a08d36a0bb0e7be4b3d80
muratilgun <muratilgun34@gmail.com>
2022-06-06 08:29:13 +0300

    Create CatalogService.cs

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,35 @@
+﻿using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
