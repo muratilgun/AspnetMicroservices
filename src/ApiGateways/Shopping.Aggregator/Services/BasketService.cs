746c78935d9a4987fa639ed885f5ac250977e120
muratilgun <muratilgun34@gmail.com>
2022-06-06 08:34:04 +0300

    Services registered

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,22 @@
+﻿using Shopping.Aggregator.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
