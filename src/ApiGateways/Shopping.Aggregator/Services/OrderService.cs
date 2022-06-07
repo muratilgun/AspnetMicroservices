1fb74b3a35f6a5593381f594af4e90f45c9fc2c4
muratilgun <muratilgun34@gmail.com>
2022-06-07 11:19:32 +0300

    Implemented OrderService

--- src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System;
+using Shopping.Aggregator.Extensions;
 
 namespace Shopping.Aggregator.Services
 {
@@ -17,7 +18,8 @@ namespace Shopping.Aggregator.Services
 
         public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
